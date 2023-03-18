59734ef25cb6e27b1b91fa4f45e206fff2bbc5c7
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-03-18 16:18:13 +0000

    added props for first card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import star from '../assets/star.png'
-import katie from '../assets/katie-zaferes.png'
 
-export default function Card() {
+export default function Card(props) {
     return(
         <div className="card">
-            <img src={katie} className="card--image" />
+            <img src={props.img} className="card--image" />
             <div className="card--stats">
                 <img className="card--star" src={star} />
-                <span>5.0</span>
-                <span className="grey">(6) • </span>
-                <span className="grey">USA</span>
+                <span>{props.rating}</span>
+                <span className="grey">({props.reviewCount}) • </span>
+                <span className="grey">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span>  / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span>  / person</p>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,20 @@ import "./index.css";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import Card from "./components/card";
+import katie from './assets/katie-zaferes.png'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Navbar />
     <Hero />
-    <Card />
+    <Card
+      img={katie}
+      rating='5.0'
+      reviewCount={6}
+      country="USA"
+      title="Life Lessons with Katie Zaferes"
+      price={136}
+    />
     <App />
   </React.StrictMode>
 );
