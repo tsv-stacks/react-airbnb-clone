0a75c4bf6ac81b2ea45e3ea7bb0f6ed798a84e33
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-03-19 15:56:41 +0000

    finished airbnb-clone

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,17 @@ import cardData from './assets/card-images/data.jsx'
 import Card from "./components/card";
 
 function App() {
-  console.log(cardData);
   const cardApp = cardData.map(card => {
-    console.log(card.coverImg);
     return (<Card
-      img={card.coverImg}
-      rating={card.stats.rating}
-      reviewCount={card.stats.reviewCount}
-      location={card.location}
-      title={card.title}
-      price={card.price}
+      key={card.id}
+      card={card}
     />)
   })
-  console.log(cardApp);
-  return cardApp;
+  return (
+    <section className="cards-list">
+      {cardApp}
+    </section>
+  )
 }
 
 export default App;
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import star from '../assets/star.png'
 
 export default function Card(props) {
+    let badgeText
+   props = props.card
+    if (props.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.location === 'Online') {
+        badgeText = 'Online'
+    }
+
     return(
         <div className="card">
-            <img src={props.img} className="card--image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={props.coverImg} className="card--image" />
             <div className="card--stats">
                 <img className="card--star" src={star} />
-                <span>{props.rating}</span>
-                <span className="grey">({props.reviewCount}) • </span>
+                <span>{props.stats.rating}</span>
+                <span className="grey">({props.stats.reviewCount}) • </span>
                 <span className="grey"> {props.location}</span>
             </div>
             <p>{props.title}</p>
